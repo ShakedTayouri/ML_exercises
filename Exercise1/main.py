00b40ab683ce1f428fe3f5850d3246e4e1f89053
tayouris <tayouris@post.bgu.ac.il>
2024-12-17 22:36:27 +0200

    data analysis base on 5 datasets

--- Exercise1/main.py
+++ Exercise1/main.py
@@ -1,30 +1,80 @@
 import numpy as np
+import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
 
 from Exercise1.SoftDecisionTreeClassifier import SoftDecisionTreeClassifier
 
-if __name__ == '__main__':
-    data = load_iris()
-    X = data.data
-    y = data.target
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+def compare_soft_and_regular_decision_tree(x, y, max_depth=None, min_samples_leaf=1, min_samples_split=2,
+                                           random_state=None):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
 
-    # Use the origin classifier to compare
-    # clf = DecisionTreeClassifier(random_state=42, max_depth=5, min_samples_leaf=1, min_samples_split=10)
+    # origin classifier to compare
+    clf = DecisionTreeClassifier(random_state=random_state, max_depth=max_depth, min_samples_leaf=min_samples_leaf,
+                                 min_samples_split=min_samples_split, )
+    predict_accuries(clf, x_test, x_train, y_test, y_train)
 
-    # When alpha high the accuracy is low
-    clf = SoftDecisionTreeClassifier(alpha=0.1, n_samples=10, random_state=42, max_depth=5, min_samples_leaf=1,
+    clf = SoftDecisionTreeClassifier(alpha=0.1, n_samples=10, random_state=random_state, max_depth=max_depth,
+                                     min_samples_leaf=min_samples_leaf,
                                      min_samples_split=10)
-    clf.fit(X_train, y_train)
+    predict_accuries(clf, x_test, x_train, y_test, y_train)
 
-    proba_train = clf.predict_proba(X_train)
+
+def predict_accuries(clf, x_test, x_train, y_test, y_train):
+    clf.fit(x_train, y_train)
+    proba_train = clf.predict_proba(x_train)
     y_pred_labels_train = np.argmax(proba_train, axis=1)
-
-    proba_test = clf.predict_proba(X_test)
+    proba_test = clf.predict_proba(x_test)
     y_pred_labels_test = np.argmax(proba_test, axis=1)
-
     print('score on training: {score}'.format(score=accuracy_score(y_train, y_pred_labels_train)))
     print('score on test: {score}'.format(score=accuracy_score(y_test, y_pred_labels_test)))
+
+
+if __name__ == '__main__':
+    data = load_iris()
+    X, Y = data.data, data.target
+    print("Iris Dataset:")
+    compare_soft_and_regular_decision_tree(X, Y, 6, 1, 2)
+    print("\n")
+
+    # 31 features, 569 samples
+    data = pd.read_csv(
+        'C:\\Users\\shake\\PycharmProjects\\ML_exs\\Exercise1\\datasets\\breast_cancer_wisconsin_diagnostic.csv')
+    X, Y = data.drop(columns=['diagnosis']), data['diagnosis']
+    label_encoder = LabelEncoder()
+    Y = label_encoder.fit_transform(Y)
+
+    print("Breast Cancer Wisconsin Diagnostic Dataset:")
+    compare_soft_and_regular_decision_tree(X, Y, 6, 1, 2)
+    print("\n")
+
+    # 13 features and 303 samples
+    data = pd.read_csv(
+        'C:\\Users\\shake\\PycharmProjects\\ML_exs\\Exercise1\\datasets\\Heart_disease_cleveland_new.csv')
+    X, Y = data.drop(columns=['target']), data['target']
+
+    print("Heart Disease Cleveland Dataset\n")
+    compare_soft_and_regular_decision_tree(X, Y, 7, 5, 2)
+    print("\n")
+
+    # 54 features, 1315 samples
+    data = pd.read_csv(
+        'C:\\Users\\shake\\PycharmProjects\\ML_exs\\Exercise1\\datasets\\covtype.csv')
+    X, Y = data.drop(columns=['Cover_Type']), data['Cover_Type']
+
+    print("Covtype Dataset\n")
+    compare_soft_and_regular_decision_tree(X, Y, 6, 1, 2)
+    print("\n")
+
+    # 8 features, 768 samples
+    data = pd.read_csv(
+        'C:\\Users\\shake\\PycharmProjects\\ML_exs\\Exercise1\\datasets\\diabetes.csv')
+    X, Y = data.drop(columns=['Outcome']), data['Outcome']
+
+    print("Diabetes Dataset\n")
+    compare_soft_and_regular_decision_tree(X, Y, 6, 1, 2)
+    print("\n")
